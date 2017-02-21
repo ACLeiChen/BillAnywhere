1fca70a482e0b8f95d81d11f6837d5bef1b48c97
Lei Chen <c1520169286@gmail.com>
2017-02-22 00:00:21 +0100

    add a button to take a photo, then show the thumbnail

--- app/src/main/java/com/example/leichen/billanywhere/MainActivity.java
+++ app/src/main/java/com/example/leichen/billanywhere/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.leichen.billanywhere;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements HeadlinesFragment.OnHeadlineSelectedListener {
     public final static String EXTRA_MESSAGE = "com.example.leichen.billanywhere.MESSAGE";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageView1);
 
         // However, if we're being restored from a previous state,
         // then we don't need to do anything and should return or else
@@ -62,6 +70,22 @@ public class MainActivity extends AppCompatActivity implements HeadlinesFragment
 
         // Commit the transaction
         transaction.commit();
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
 
