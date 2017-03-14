b7f1c1d431206012a4b6ae2e7e7c737fc024de6f
EVISION\L.Chen <l.chen@evision-software.com>
2017-03-14 14:26:32 +0100

    add ActivityContext

---
+++ app/src/main/java/com/example/leichen/billanywhere/di/ActivityContext.java
@@ -0,0 +1,15 @@
+package com.example.leichen.billanywhere.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Lei Chen on 2017/3/15.
+ */
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityContext {
+}
