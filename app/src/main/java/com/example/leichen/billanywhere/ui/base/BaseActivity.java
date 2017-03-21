e6813ae7fc4c824832071d44c0a4e2b6091c4691
Lei Chen <c1520169286@gmail.com>
2017-03-21 23:34:18 +0100

    Add MvpView and BaseActivity, include onError()

---
+++ app/src/main/java/com/example/leichen/billanywhere/ui/base/BaseActivity.java
@@ -0,0 +1,43 @@
+package com.example.leichen.billanywhere.ui.base;
+
+import android.support.annotation.StringRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.leichen.billanywhere.R;
+
+public class BaseActivity extends AppCompatActivity implements MvpView {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onError(@StringRes int resId) {
+        onError(getString(resId));
+    }
+
+    @Override
+    public void onError(String message) {
+        if (message != null) {
+            showSnackBar(message);
+        } else {
+            showSnackBar(getString(R.string.some_error));
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                message, Snackbar.LENGTH_SHORT);
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView
+                .findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(ContextCompat.getColor(this, R.color.white));
+        snackbar.show();
+    }
+}
