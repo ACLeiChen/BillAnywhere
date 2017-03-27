b8c28b13cf24b0149254fd0682ef086e399d339f
Lei Chen <c1520169286@gmail.com>
2017-03-27 23:36:07 +0200

    update bill model, associate it with greenDao.Methods need to be added to AppDbHelper.

--- app/src/main/java/com/example/leichen/billanywhere/data/model/Bill.java
+++ app/src/main/java/com/example/leichen/billanywhere/data/model/Bill.java
@@ -1,21 +1,58 @@
 
 package com.example.leichen.billanywhere.data.model;
 
+import java.util.Date;
 import java.util.List;
+
+import com.example.leichen.billanywhere.data.DataManager;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.ToMany;
+import org.greenrobot.greendao.DaoException;
+
+@Entity(nameInDb = "bills")
 public class Bill {
 
-    @SerializedName("items")
     @Expose
-    private List<Item> items = null;
+    @SerializedName("id")
+    @Id
+    private Long id;
+
     @SerializedName("date")
     @Expose
-    private String date;
+    @Property(nameInDb = "shopping_date")
+    private Date shopping_date;
+
     @SerializedName("shop_name")
     @Expose
+    @Property(nameInDb = "shop_name")
     private String shopName;
+
+    @ToMany(referencedJoinProperty = "billId")
+    private List<Item> items = null;
+
+    @Expose
+    @SerializedName("created_at")
+    @Property(nameInDb = "created_at")
+    private String createdAt;
+
+    @Expose
+    @SerializedName("updated_at")
+    @Property(nameInDb = "updated_at")
+    private String updatedAt;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 318225070)
+    private transient BillDao myDao;
 
     /**
      * No args constructor for use in serialization
@@ -24,33 +61,51 @@ public class Bill {
     public Bill() {
     }
 
-    /**
-     * 
-     * @param items
-     * @param shopName
-     * @param date
-     */
-    public Bill(List<Item> items, String date, String shopName) {
-        super();
-        this.items = items;
-        this.date = date;
+
+    @Generated(hash = 1941473591)
+    public Bill(Long id, Date shopping_date, String shopName, String createdAt,
+            String updatedAt) {
+        this.id = id;
+        this.shopping_date = shopping_date;
         this.shopName = shopName;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
+
+    /**
+     * To-many relationship, resolved on first access (and after reset).
+     * Changes to to-many relations are not persisted, make changes to the target entity.
+     */
+    @Generated(hash = 898253763)
     public List<Item> getItems() {
+        if (items == null) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            ItemDao targetDao = daoSession.getItemDao();
+            List<Item> itemsNew = targetDao._queryBill_Items(id);
+            synchronized (this) {
+                if (items == null) {
+                    items = itemsNew;
+                }
+            }
+        }
         return items;
     }
+
 
     public void setItems(List<Item> items) {
         this.items = items;
     }
 
-    public String getDate() {
-        return date;
+    public Date getDate() {
+        return shopping_date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setDate(Date shopping_date) {
+        this.shopping_date = shopping_date;
     }
 
     public String getShopName() {
@@ -61,4 +116,93 @@ public class Bill {
         this.shopName = shopName;
     }
 
+    public String getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Date getShopping_date() {
+        return this.shopping_date;
+    }
+
+
+    public void setShopping_date(Date shopping_date) {
+        this.shopping_date = shopping_date;
+    }
+
+
+    /** Resets a to-many relationship, making the next get call to query for a fresh result. */
+    @Generated(hash = 1727286264)
+    public synchronized void resetItems() {
+        items = null;
+    }
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 173933155)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getBillDao() : null;
+    }
 }
--- app/src/main/java/com/example/leichen/billanywhere/data/model/Item.java
+++ app/src/main/java/com/example/leichen/billanywhere/data/model/Item.java
@@ -4,23 +4,62 @@ package com.example.leichen.billanywhere.data.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.ToOne;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.DaoException;
+import org.greenrobot.greendao.annotation.NotNull;
+
+@Entity(nameInDb = "items")
 public class Item {
+
+    @Expose
+    @SerializedName("id")
+    @Id
+    private Long id;
 
     @SerializedName("item_name")
     @Expose
+    @Property(nameInDb = "item_name")
     private String itemName;
+
     @SerializedName("quantity")
     @Expose
+    @Property(nameInDb = "quantity")
     private Integer quantity;
+
     @SerializedName("rate")
     @Expose
+    @Property(nameInDb = "rate")
     private Integer rate;
+
     @SerializedName("amount")
     @Expose
+    @Property(nameInDb = "amount")
     private Integer amount;
+
     @SerializedName("kind")
     @Expose
+    @Property(nameInDb = "kind")
     private String kind;
+
+    @ToOne(joinProperty = "billId")
+    private Bill bill;
+
+    private long billId;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 182764869)
+    private transient ItemDao myDao;
+
+    @Generated(hash = 1276473913)
+    private transient Long bill__resolvedKey;
 
     /**
      * No args constructor for use in serialization
@@ -29,22 +68,21 @@ public class Item {
     public Item() {
     }
 
-    /**
-     * 
-     * @param amount
-     * @param itemName
-     * @param rate
-     * @param quantity
-     * @param kind
-     */
-    public Item(String itemName, Integer quantity, Integer rate, Integer amount, String kind) {
-        super();
+
+
+    @Generated(hash = 290751382)
+    public Item(Long id, String itemName, Integer quantity, Integer rate,
+            Integer amount, String kind, long billId) {
+        this.id = id;
         this.itemName = itemName;
         this.quantity = quantity;
         this.rate = rate;
         this.amount = amount;
         this.kind = kind;
+        this.billId = billId;
     }
+
+
 
     public String getItemName() {
         return itemName;
@@ -84,6 +122,118 @@ public class Item {
 
     public void setKind(String kind) {
         this.kind = kind;
+    }
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public long getBillId() {
+        return this.billId;
+    }
+
+
+
+    public void setBillId(long billId) {
+        this.billId = billId;
+    }
+
+
+
+    /** To-one relationship, resolved on first access. */
+    @Generated(hash = 993835375)
+    public Bill getBill() {
+        long __key = this.billId;
+        if (bill__resolvedKey == null || !bill__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            BillDao targetDao = daoSession.getBillDao();
+            Bill billNew = targetDao.load(__key);
+            synchronized (this) {
+                bill = billNew;
+                bill__resolvedKey = __key;
+            }
+        }
+        return bill;
+    }
+
+
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 601743104)
+    public void setBill(@NotNull Bill bill) {
+        if (bill == null) {
+            throw new DaoException(
+                    "To-one property 'billId' has not-null constraint; cannot set to-one to null");
+        }
+        synchronized (this) {
+            this.bill = bill;
+            billId = bill.getId();
+            bill__resolvedKey = billId;
+        }
+    }
+
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 881068859)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getItemDao() : null;
     }
 
 }
