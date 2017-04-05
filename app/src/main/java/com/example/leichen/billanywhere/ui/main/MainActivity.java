c89056c4ff0bfb1dc16b4830f5cd148a6a2ff49f
Lei Chen <c1520169286@gmail.com>
2017-04-05 23:36:13 +0200

    Bind view for mainActivity

--- app/src/main/java/com/example/leichen/billanywhere/ui/main/MainActivity.java
+++ app/src/main/java/com/example/leichen/billanywhere/ui/main/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity implements MainMvpView{
 
@@ -34,22 +35,24 @@ public class MainActivity extends BaseActivity implements MainMvpView{
     @Override
     protected void setUp() {
         billsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        //billsRecyclerView.setAdapter();
         mPresenter.onViewInitialized();
     }
 
     @Override
+    @OnClick(R.id.plusButton)
     public void onPlusButtonClick() {
     }
 
+    //OnClick or OnItemClick?, needs more reading.
     @Override
+    @OnClick({R.id.bill_simple, R.id.timestamp, R.id.shopping_date, R.id.total_amount})
     public void onOldBillItemClick() {
 
     }
 
     @Override
     public void refreshBills(List<Bill> bills) {
-
+        billsRecyclerView.setAdapter(new BillsRecyclerViewAdapter(bills, this));
     }
 
 
